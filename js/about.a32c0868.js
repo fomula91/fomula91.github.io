c602a878f6cd735b0891b0407c94665f331a3872
fomula91 <fomula91@gmail.com>
2021-08-07 23:41:50 +0900

    vue router

---
+++ js/about.a32c0868.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{e348:function(n,e,t){"use strict";t.r(e);var o=t("7a23"),c=Object(o["f"])("h1",null,"helo!!!?!?!?!?!?!?!?!?",-1);function u(n,e){return Object(o["o"])(),Object(o["d"])("div",null,[c])}const r={};r.render=u;e["default"]=r}}]);
+//# sourceMappingURL=about.a32c0868.js.map
