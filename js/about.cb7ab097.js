e1042a94625c1845b0784289b23b5d21ead173ae
fomula91 <fomula91@gmail.com>
2021-08-15 04:12:32 +0900

    bootstrap testpage

---
+++ js/about.cb7ab097.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{e348:function(n,e,t){"use strict";t.r(e);var c=t("7a23"),o=Object(c["g"])("h1",null,"helo!!!?!?!?!?!?!?!?!?",-1);function u(n,e){return Object(c["p"])(),Object(c["d"])("div",null,[o])}const r={};r.render=u;e["default"]=r}}]);
+//# sourceMappingURL=about.cb7ab097.js.map
